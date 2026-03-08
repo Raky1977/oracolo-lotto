727ffcb334f8d42296cec87e94fdd1920dcbd5fb
Raky1977 <cry.molteni@gmail.com>
2026-03-08 18:28:08 +0100

    Update update_data.py

--- scripts/update_data.py
+++ scripts/update_data.py
@@ -2,9 +2,6 @@ import requests
 
 url = "https://lotto-api.onrender.com/lotto/latest"
 
-try:
-    r = requests.get(url, timeout=10)
-    print("Status:", r.status_code)
-    print("Risposta:", r.text)
-except Exception as e:
-    print("Errore:", e)
+r = requests.get(url, timeout=10)
+print(r.status_code)
+print(r.text)
